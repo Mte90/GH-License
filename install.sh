b3fae8af313e1f6013963b99bd03c13354b9b098
Daniele Scasciafratte <mte90net@gmail.com>
2016-02-06 18:10:35 +0100

    working on install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+wget https://github.com/Mte90/GH-License/blob/master/gh-license.py /usr/local/bin/gh-license
+
+pip3 install pygithub
+
+echo 'gh-license script install use: gh-license'
