ea2bd72b150d48d899b73c525b8d390e719c7e8c
Daniele Scasciafratte <mte90net@gmail.com>
2016-10-25 11:41:28 +0200

    prepare for 0.2

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 from setuptools import setup, find_packages
 
 import os
@@ -6,11 +8,12 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 setup(
     name="gh-license",
-    version="0.1a",
+    version="0.2",
     author="Mte90",
     install_requires=open(os.path.join(BASE_DIR, "requirements.txt")).readlines(),
     license="GPLv3",
     description="Scan github repositories for missing license files and add them",
+    download_url = 'https://github.com/Mte90/GH-License/tarball/0.2',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
