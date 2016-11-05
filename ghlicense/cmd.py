c322eb72060ed1fb8c671ba2d9c90ab675933ad3
Daniele Scasciafratte <mte90net@gmail.com>
2016-11-05 12:44:19 +0100

    fix the missing download

--- ghlicense/cmd.py
+++ ghlicense/cmd.py
@@ -24,7 +24,8 @@ parser = argparse.ArgumentParser(description = "GitHosting License checker and d
     epilog = enhanced_description, formatter_class=RawTextHelpFormatter)
 err_providers_txt = "(errored providers: %s)" % ", ".join(disabled_providers) if len(disabled_providers) > 0 else ""
 parser.add_argument("--scan", help="Scan repo of the user, arguments: [User_nick]", action="store")
-parser.add_argument("--license", help="Download a license file, arguments: [License_name]", action="store_true")
+parser.add_argument("--license", help="Download a license file, arguments: [License_name]", action="store")
+parser.add_argument("--licenselist", help="Show licenses available", action="store_true")
 parser.add_argument("--provider", help="Repository provider. Defaults to github. Available providers: %s %s" % 
     (", ".join(enabled_providers), err_providers_txt), action="store", default="github")
 parser.add_argument("--report", help="The report filename for scan (optional)", action="store")
@@ -50,11 +51,12 @@ def progressBar(current, total):
     sys.stdout.flush()
 # Download the license
 def downloadLicense(url, name, badge):
+    print('License ' + name + ' download in progress.')    
     if not os.path.isfile("LICENSE"):
         urllib.request.urlretrieve(url, "LICENSE")
         print('License ' + name + ' downloaded with filename LICENSE.')
     # Search for the readme
-    readme_files = ['README.md','README.txt','readme','README','readme.txt','readme.md']
+    readme_files = ['README.md','Readme.md','README.txt','readme','README','readme.txt','readme.md']
     for readme_file in readme_files:
         if os.path.isfile(readme_file):
             f = open(readme_file, 'r+')
@@ -72,7 +74,7 @@ def downloadLicense(url, name, badge):
                 os.system('git add LICENSE')
                 os.system('git add ' + readme_file)
                 os.system("git commit -m 'missing LICENSE'")
-                if args.origin == None:
+                if args.origin != None:
                     os.system('git push ' + args.origin + ' master')
                 else:
                     os.system('git push origin master')
@@ -133,10 +135,8 @@ def main():
         report_file.write("Total Repos: " + str(count_no_license + count_license) + "\n")
         report_file.close()
 
-    elif args.license:
-        if args.license == True:
-             sys.stderr.write('  First parameter is missing: The license:'\
-             '\n  GPLv2\n'\
+    elif args.licenselist == True:
+             sys.stderr.write('\n  GPLv2\n'\
              '\tYou may copy, distribute and modify the software.\n'\
              '\tAny modifications must also be made available under\n'\
              '\tthe GPL along with build & install instructions.'\
@@ -173,33 +173,34 @@ def main():
              '\n\n  MIT\n'\
              '\tA short, permissive software license.\n\n')
              sys.exit(1)
-
-        if args.scan == 'GPLv2':
-            downloadLicense("http://www.gnu.org/licenses/gpl-2.0.txt", args.scan, '(https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://img.shields.io/badge/License-GPL%20v2-blue.svg)')
-        elif args.scan == 'GPLv3':
-            downloadLicense("http://www.gnu.org/licenses/gpl-3.0.txt", args.scan, '(https://img.shields.io/badge/License-GPL%20v3-blue.svg)](http://www.gnu.org/licenses/gpl-3.0)')
-        elif args.scan == 'LGPLv3':
-            downloadLicense("http://www.gnu.org/licenses/lgpl-3.0.txt", args.scan, '(https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](http://www.gnu.org/licenses/lgpl-3.0)')
-        elif args.scan == 'AGPLv3':
-            downloadLicense("http://www.gnu.org/licenses/agpl-3.0.txt", args.scan, '(https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](http://www.gnu.org/licenses/agpl-3.0)')
-        elif args.scan == 'FDLv1.3':
-            downloadLicense("http://www.gnu.org/licenses/fdl-1.3.txt", args.scan, '(https://img.shields.io/badge/License-FDL%20v1.3-blue.svg)](http://www.gnu.org/licenses/fdl-1.3)')
-        elif args.scan == 'Apachev2':
-            downloadLicense("http://www.opensource.apple.com/source/apache2/apache2-19/apache2.txt?txt", args.scan, '(https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)')
-        elif args.scan == 'CC-BY':
-            downloadLicense("http://creativecommons.org/licenses/by/3.0/legalcode.txt", args.scan, '(https://img.shields.io/badge/License-CC%20BY%204.0-lightgrey.svg)(http://creativecommons.org/licenses/by/4.0/)')
-        elif args.scan == 'BSDv2':
-            downloadLicense("https://spdx.org/licenses/BSD-2-Clause.txt", args.scan, '(https://img.shields.io/badge/License-BSD%20v2-blue.svg)](https://spdx.org/licenses/BSD-2-Clause)')
-        elif args.scan == 'BSDv3':
-            downloadLicense("https://spdx.org/licenses/BSD-3-Clause.txt", args.scan, '(https://img.shields.io/badge/License-BSD%20v3-blue.svg)](https://spdx.org/licenses/BSD-3-Clause)')
-        elif args.scan == 'BSDv4':
-            downloadLicense("https://spdx.org/licenses/BSD-4-Clause.txt", args.scan, '(https://img.shields.io/badge/License-BSD%20v4-blue.svg)](https://spdx.org/licenses/BSD-4-Clause)')
-        elif args.scan == 'MPLv2':
-            downloadLicense("https://www.mozilla.org/media/MPL/2.0/index.815ca599c9df.txt", args.scan, '(https://img.shields.io/badge/License-MozillaPublicLicense%20v2-blue.svg)](https://www.mozilla.org/en-US/MPL/2.0)')
-        elif args.scan == 'UNLICENSE':
-            downloadLicense("http://unlicense.org/UNLICENSE", args.scan, '(https://img.shields.io/badge/License-UNLICENSE%20v1-blue.svg)](http://unlicense.org/UNLICENSE)')
-        elif args.scan == 'MIT':
-            downloadLicense("https://spdx.org/licenses/MIT.txt", args.scan, '(https://img.shields.io/badge/License-MIT%20v1-blue.svg)](https://spdx.org/licenses/MIT.html#licenseText)')
+             
+    elif args.license:
+        if args.license == 'GPLv2':
+            downloadLicense("http://www.gnu.org/licenses/gpl-2.0.txt", args.license, '(https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://img.shields.io/badge/License-GPL%20v2-blue.svg)')
+        elif args.license == 'GPLv3':
+            downloadLicense("http://www.gnu.org/licenses/gpl-3.0.txt", args.license, '(https://img.shields.io/badge/License-GPL%20v3-blue.svg)](http://www.gnu.org/licenses/gpl-3.0)')
+        elif args.license == 'LGPLv3':
+            downloadLicense("http://www.gnu.org/licenses/lgpl-3.0.txt", args.license, '(https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](http://www.gnu.org/licenses/lgpl-3.0)')
+        elif args.license == 'AGPLv3':
+            downloadLicense("http://www.gnu.org/licenses/agpl-3.0.txt", args.license, '(https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](http://www.gnu.org/licenses/agpl-3.0)')
+        elif args.license == 'FDLv1.3':
+            downloadLicense("http://www.gnu.org/licenses/fdl-1.3.txt", args.license, '(https://img.shields.io/badge/License-FDL%20v1.3-blue.svg)](http://www.gnu.org/licenses/fdl-1.3)')
+        elif args.license == 'Apachev2':
+            downloadLicense("http://www.opensource.apple.com/source/apache2/apache2-19/apache2.txt?txt", args.license, '(https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)')
+        elif args.license == 'CC-BY':
+            downloadLicense("http://creativecommons.org/licenses/by/3.0/legalcode.txt", args.license, '(https://img.shields.io/badge/License-CC%20BY%204.0-lightgrey.svg)(http://creativecommons.org/licenses/by/4.0/)')
+        elif args.license == 'BSDv2':
+            downloadLicense("https://spdx.org/licenses/BSD-2-Clause.txt", args.license, '(https://img.shields.io/badge/License-BSD%20v2-blue.svg)](https://spdx.org/licenses/BSD-2-Clause)')
+        elif args.license == 'BSDv3':
+            downloadLicense("https://spdx.org/licenses/BSD-3-Clause.txt", args.license, '(https://img.shields.io/badge/License-BSD%20v3-blue.svg)](https://spdx.org/licenses/BSD-3-Clause)')
+        elif args.license == 'BSDv4':
+            downloadLicense("https://spdx.org/licenses/BSD-4-Clause.txt", args.license, '(https://img.shields.io/badge/License-BSD%20v4-blue.svg)](https://spdx.org/licenses/BSD-4-Clause)')
+        elif args.license == 'MPLv2':
+            downloadLicense("https://www.mozilla.org/media/MPL/2.0/index.815ca599c9df.txt", args.license, '(https://img.shields.io/badge/License-MozillaPublicLicense%20v2-blue.svg)](https://www.mozilla.org/en-US/MPL/2.0)')
+        elif args.license == 'UNLICENSE':
+            downloadLicense("http://unlicense.org/UNLICENSE", args.license, '(https://img.shields.io/badge/License-UNLICENSE%20v1-blue.svg)](http://unlicense.org/UNLICENSE)')
+        elif args.license == 'MIT':
+            downloadLicense("https://spdx.org/licenses/MIT.txt", args.license, '(https://img.shields.io/badge/License-MIT%20v1-blue.svg)](https://spdx.org/licenses/MIT.html#licenseText)')
         else:
             print('License not found!')    
 
