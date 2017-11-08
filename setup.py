bd4b8c5d8a663802e6389722d866756aedc454e1
Daniele Scasciafratte <mte90net@gmail.com>
2017-11-08 18:55:01 +0100

    little fix on upload new version system

--- setup.py
+++ setup.py
@@ -11,6 +11,8 @@ setup(
     name="gh-license",
     version="0.2.4",
     author="Mte90",
+    author_email="mte90net@gmail.com",
+    url="https://github.com/Mte90/GH-License",
     install_requires=open(os.path.join(BASE_DIR, "requirements.txt")).readlines(),
     license="GPLv3",
     description="Scan github, bitbucket or other providers repositories for missing license files and add them",
