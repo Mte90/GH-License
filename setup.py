a6a52c031e5684e284c7a41fa6f0e054fdd024e7
Daniele Scasciafratte <mte90net@gmail.com>
2017-12-20 23:29:59 +0100

    bump

--- setup.py
+++ setup.py
@@ -9,14 +9,14 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 setup(
     name="gh-license",
-    version="0.2.5",
+    version="0.2.6",
     author="Mte90",
     author_email="mte90net@gmail.com",
     url="https://github.com/Mte90/GH-License",
     install_requires=open(os.path.join(BASE_DIR, "requirements.txt")).readlines(),
     license="GPLv3",
     description="Scan github, bitbucket or other providers repositories for missing license files and add them",
-    download_url='https://github.com/Mte90/GH-License/tarball/0.2.2',
+    download_url='https://github.com/Mte90/GH-License/tarball/0.2.6',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
